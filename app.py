a982c087f4d93c1d32b202d9337ed25cdc661525
mayurd1808 <mayur.d1808@gmail.com>
2026-04-21 12:53:51 +0530

    Fixed Gemini model

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if not GEMINI_API_KEY:
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("models/gemini-1.5-flash-latest")
+model = genai.GenerativeModel("gemini-1.5-flash")
 # --------------------
 # Load Course Data
 # --------------------
